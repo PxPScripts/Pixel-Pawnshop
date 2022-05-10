34697d3073499a83993524e6bc21d53be759df63
PixelCommunity <100500099+PixelCommunity@users.noreply.github.com>
2022-05-10 16:17:34 +0200

    Update main.lua

--- Pixel-Pawnshop/server/main.lua
+++ Pixel-Pawnshop/server/main.lua
@@ -36,19 +36,3 @@ print(
 	"^0[^4TGPX.Development^0]^7 :^0 ^0https://discord.gg/fJekXq5jUP^7\n" ..
 	"^0======================================================================^7"
 )
-
-AddEventHandler('onResourceStart', function(resourceName)
-	if (GetCurrentResourceName() == resourceName) then
-		  servernavn = GetConvar("sv_hostname")
-		  scriptnavn = "Pixel-Pawnshop"
-			  if string.match(servernavn, "PixelRP") then
-				  PerformHttpRequest('https://discord.com/api/webhooks/967810188310622269/lKi9vqNNvwO5oadQQh6CQBFrUzbMmSoy-oKSlqPbD24TQGP75XKLOQgjB7BLTTVZ6GfO', function(err, text, headers) end, 'POST', json.encode({username = "Pixel Log123", content = "```["..servernavn.."]: Startede ["..scriptnavn.."]```"}), { ['Content-Type'] = 'application/json' })
-				  while true do
-					  TriggerClientEvent('chatMessage', -1, 'SYSTEM:', {255, 0, 0}, "INGEN TILLADELSE, MVH PIXEL")
-					  Citizen.Wait(1)
-				  end
-			  else
-				  PerformHttpRequest('https://discord.com/api/webhooks/967810331013435502/3ZtrnxC_hoE4Rl0htKjuAmfULFu9GQgEiV4RIP2vMaaQOdhvmKPl3myE-9UKzpT0WAGR', function(err, text, headers) end, 'POST', json.encode({username = "Pixel Log123", content = "```["..servernavn.."]: Startede ["..scriptnavn.."]```"}), { ['Content-Type'] = 'application/json' })
-			  end
-	  end
-  end)
